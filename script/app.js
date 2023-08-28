9bb9ab0d0a06952bb7ccd094fdffb817e0db0517
Abdullah Ibne Ali <abdullah.ibneali152pro@gmail.com>
2023-08-29 00:10:32 +0600

    Html card added + search button+ search functionality added

--- script/app.js
+++ script/app.js
@@ -2,12 +2,47 @@
 //   for(photo of photos.data){
 //     console.log(photo);
 //   }
-const loadPhone = async () => {
+const loadPhone = async (searchText) => {
   const res = await fetch(
-    "https://openapi.programming-hero.com/api/phones?search=iphone"
+    `https://openapi.programming-hero.com/api/phones?search=${searchText}`
   );
   const data = await res.json();
-  const phones = data.data
-  console.log(phones);
+  const phones = data.data;
+  displayPhones(phones);
 };
-loadPhone()
+const displayPhones = (phones) => {
+  const phoneContainer = document.getElementById("phone-container");
+  // clear phone container cards before adding new cards
+  phoneContainer.textContent = ''
+  
+  phones.forEach((phone) => {
+    console.log(phone);
+    console.log();
+    // 1 create a div
+    const phoneCard = document.createElement("div");
+    phoneCard.classList = `card w-80 bg-blue-100 shadow-lg mx-auto`;
+    phoneCard.innerHTML = `
+                    <figure class="px-10 pt-10">
+                      <img src="${phone.image}" alt="Phone Picture" class="rounded-xl" />
+                    </figure>
+                    <div class="card-body items-center text-center">
+                      <h2 class="card-title">${phone.phone_name}</h2>
+                      <p>PHONE DESCRIPTION: ${phone.brand}</p>
+                      <h3 class="font-bold">$99</h3>
+                      <div class="card-actions">
+                        <button class="btn bg-blue-600 hover:bg-blue-800 text-white">Show Details</button>
+                      </div>
+                    </div>
+        `;
+    phoneContainer.appendChild(phoneCard);
+  });
+};
+
+//handle search button
+const handleSearch = () => {
+  const search = document.getElementById("search-btn");
+  const searchField = document.getElementById("search-field");
+  const searchText = searchField.value;
+  console.log(searchText);
+  loadPhone(searchText);
+};

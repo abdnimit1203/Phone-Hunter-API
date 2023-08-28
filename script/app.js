8e6d9946c05d95cea8199f72dc413ad43cd38923
Abdullah Ibne Ali <abdullah.ibneali152pro@gmail.com>
2023-08-28 21:35:46 +0600

    All files created and api connected

---
+++ script/app.js
@@ -0,0 +1,13 @@
+//   const ul = document.getElementById("photo-list");
+//   for(photo of photos.data){
+//     console.log(photo);
+//   }
+const loadPhone = async () => {
+  const res = await fetch(
+    "https://openapi.programming-hero.com/api/phones?search=iphone"
+  );
+  const data = await res.json();
+  const phones = data.data
+  console.log(phones);
+};
+loadPhone()
